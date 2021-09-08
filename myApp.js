8918f61cead621dfc52693c5dccd35d575ef0a9f
Mervin Villaceran <mervinvillaceran@Mervins-MacBook-Pro.local>
2021-09-08 18:33:05 +0800

    json response

--- myApp.js
+++ myApp.js
@@ -7,6 +7,10 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/json', function(req, res) {
+    res.json({ "message": "Hello json" });
+});
+
 console.log("Hello World");
 
 
